8f2f1fd99c805bebdb3b34a199cd918da3946323
Jacek Becela <jacek.becela@gmail.com>
2012-06-14 11:16:57 +0200

    rails new exbug

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exbug::Application.config.secret_token = '0bc9c8a32c1af41d0f83fa48d5d0be95f7d9b0ff7870108e4ba2ea99f9a55dfbe36a12a60f9a8a0a0a075c78958b3bd70567ac1eacef553e0e2bf3829ebd9621'
